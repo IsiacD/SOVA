9020e965e5917e8f6c9f130574f004669d64379c
IsiacD <46409634+IsiacD@users.noreply.github.com>
2022-08-09 10:53:46 -0500

    Add files via upload

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+opening_text = [
+    "Cool, I'm on it sir.",
+    "Okay sir, I'm working on it.",
+    "Just a second sir.",
+    "I'm looking for it now.",
+    "Coming Right up.",
+    "On it now.",
+    "Roger, tracking, hooah.",
+]
